15cd52b8b312b300e3b48280997f95dd9db6deec
Fabio Motta <fabiomotta0311@gmail.com>
2020-05-30 04:15:41 -0300

    Routes config ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,13 +14,15 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
 import { HomeComponent } from './views/home/home.component';
+import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
     NavComponent,
-    HomeComponent
+    HomeComponent,
+    ProductCrudComponent
   ],
   imports: [
     BrowserModule,
