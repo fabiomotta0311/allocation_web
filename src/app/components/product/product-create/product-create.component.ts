e98a3fdd9fcac1f310c467308964264bad7298ed
Fabio Motta <fabiomotta0311@gmail.com>
2020-05-30 06:22:38 -0300

    new product enhancements

--- src/app/components/product/product-create/product-create.component.ts
+++ src/app/components/product/product-create/product-create.component.ts
@@ -1,4 +1,7 @@
+import { Product } from './../product.model';
+import { ProductService } from './../product.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-product-create',
@@ -7,14 +10,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductCreateComponent implements OnInit {
 
-  propLegal = "qualquer"
-  constructor() { }
+  constructor(
+    private productService: ProductService,
+    private router: Router
+    ) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  createProduct(): void {
+    this.productService.showMessage('Produto criado')
   }
-
-  fazerAlgo(): void {
-    console.log('Fazendo algo!')
+  cancel(): void {
+    this.router.navigate(['/products'])
   }
 
 }
