9ff6e6a08e1caf25d695904a5c250c834fee2682
Fabio Motta <fabiomotta0311@gmail.com>
2020-05-30 03:51:56 -0300

    home view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,12 +12,14 @@ import { NavComponent } from './components/template/nav/nav.component';
 
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
+import { HomeComponent } from './views/home/home.component';
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
